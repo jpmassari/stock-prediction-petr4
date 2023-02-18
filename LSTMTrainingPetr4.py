ce4aabb719603df3fc8b9394d6589eb61c0dbdf9
João Pedro Massari <joao.massari1997@gmail.com>
2023-02-18 18:48:07 -0300

    (#feat/155740) Data manipulation
    Detalhes:
    . adicionada a função de diferença absoluta dos preços da ação
    . adicionada a função para normalizar os dados após a diffa
    . alteração da quantidade de dias analisados
    . adicionado os dados do dolar, volume de petr4, cv(coefficient
    variation), valor do barril de petroleo no treinamento do modelo
    . adicionado função contendo o algoritimo de Heap para criar subsets de
    todas as combinações possiveis do nosso range de dados para o valor
    diario petr4

--- LSTMTrainingPetr4.py
+++ LSTMTrainingPetr4.py
@@ -5,9 +5,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
-from keras.utils import normalize
-from keras.preprocessing.text import one_hot
-from keras.utils import to_categorical #one_hot
+#from keras.utils import normalize
+#from keras.preprocessing.text import one_hot
+#from keras.utils import to_categorical #one_hot
 ##pip install nvidia-cublas-cu11
 
 #print(tf.config.list_physical_devices('GPU'))
@@ -15,21 +15,65 @@ from keras.utils import to_categorical #one_hot
 #os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
 dataset = pd.read_csv('PETR4Daily.csv')
-training_set = dataset.iloc[2793:2814,1:2].values #dataset.iloc (iloc não usa gpu)
+training_set = dataset.iloc[:2814,1:2].values #dataset.iloc (iloc não usa gpu)
+training_set_petr4_volume = dataset.iloc[:2814,5:6].values
+
+dataset_dollar = pd.read_csv('USDollar.csv')
+training_set_dollar = dataset_dollar.iloc[:2814,2:3].values 
+
+dataset_oil = pd.read_csv('crudeOil.csv')
+training_set_oil = dataset_oil.iloc[:2814,2:3].values 
 
 def make_variables(initializer):
-    return (tf.Variable(initializer(shape=[19,3], dtype=tf.float64)),
-            tf.Variable(initializer(shape=[19,3], dtype=tf.float64)),
-            tf.Variable(initializer(shape=[19,3], dtype=tf.float64)))
+    return (tf.Variable(initializer(shape=[2812,8], dtype=tf.float64)),
+            tf.Variable(initializer(shape=[2812,3], dtype=tf.float64)),
+            tf.Variable(initializer(shape=[2812,8], dtype=tf.float64)))
 X_train, y_train, X_test = make_variables(tf.zeros_initializer())
 
 #feature scaling
-""" from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
-sc = MinMaxScaler(feature_range = (0,1)) """
+sc = MinMaxScaler(feature_range = (0,1))
 
 training_set = tf.constant(training_set)
-training_set_scaled = tf.Variable(training_set)
+training_set_scaled = tf.Variable(training_set, dtype=tf.float16)
+
+training_set_dollar = tf.constant(training_set_dollar)
+training_set_scaled_dollar = tf.Variable(training_set_dollar, dtype=tf.float64)
+
+training_set_oil = tf.constant(training_set_oil)
+training_set_scaled_oil = tf.Variable(training_set_oil, dtype=tf.float64)
+
+def heaps_algorithm_tf_subsets(n, k, callback):
+    c = tf.Variable(n, dtype=tf.float16)
+    p = tf.range(k, dtype=tf.int32)
+    callback(p)
+    i = 0
+    while i < k:
+        if c[i] < i:
+            if i % 2 == 0:
+                swap = 0
+            else:
+                swap = c[i]
+            p = tf.tensor_scatter_nd_update(p, [[i], [swap]], [p[swap], p[i]])
+            callback(p)
+            c = tf.tensor_scatter_nd_update(c, [[i]], [c[i] + 1])
+            i = 0
+        else:
+            c = tf.tensor_scatter_nd_update(c, [[i]], [0])
+            i += 1
+        if k < n and i == 0:
+            p = tf.concat([p, [k]])
+            k += 1
+            c = tf.concat([c, [0]])
+
+def print_permutation(perm):
+    print(perm)
+
+heaps_algorithm_tf_subsets(training_set_scaled, 2, print_permutation)
+
+def cv(x):
+    return (tf.math.reduce_std(x)/tf.math.reduce_mean(x))*100
 
 def difa(a,b):
     diff = (a-b)/b
@@ -42,25 +86,34 @@ def normalize(p, n=10, m=100):
     v = p
     nm1 = (n-1)
     v = np.round(v*m) + np.round((n-1)/2)
-    if v > nm1: 
+    if v > nm1:
         return nm1
     elif v < 0: 
         return 0 
     else: 
         return v
 
-for i in range(0, 20):
+for i in range(0, 2813):
     diff_a = difa(training_set[i,0], training_set[i+1,0])
-    p = normalize(diff_a)
-    #print(int(p))
+    p = normalize(diff_a, 10, 800)
+
+    diff_a_dollar = difa(training_set_dollar[i,0], training_set_dollar[i+1,0])
+    p_dollar = normalize(diff_a_dollar, 10, 800)
+
+    diff_a_oil = difa(training_set_oil[i,0], training_set_oil[i+1,0])
+    p_oil= normalize(diff_a_oil, 10, 800)
+
     training_set_scaled[i].assign(int(p))
+    training_set_scaled_dollar[i].assign(int(p_dollar))
+    training_set_scaled_oil[i].assign(int(p_oil))
 print("training_set_scaled: ",training_set_scaled)
 
-for i in range(0, 19):
+for i in range(0, 2812):
     for x in range(i, i+1):
         d1 = training_set_scaled[tf.size(y_train) - (tf.size(y_train) - x)]
         d2 = training_set_scaled[x + 1]
         d3 = training_set_scaled[x + 2]
+        v = cv([d1,d2])
         diff1 = 0
         diff2 = 0
         if(d2 - d1 > 0):
@@ -72,10 +125,24 @@ for i in range(0, 19):
         else:
             diff2 = float(str(2) + tf.as_string(training_set_scaled[x + 1]))
 
+        #if x % 2 == 0: #even
         y_train[x].assign([200+x, diff1, diff2])
-        X_train[x].assign([float(str(1) + tf.as_string(training_set_scaled[x])), float(str(1) + tf.as_string(training_set_scaled[x])), 100+x])
+        X_train[x].assign([
+            float(str(1) + tf.as_string(training_set_scaled[x])),
+            float(str(1) + tf.as_string(training_set_scaled[x + 1])), 
+            float(training_set_scaled_dollar[x]), 
+            float(training_set_scaled_dollar[x + 1]), 
+            float(training_set_oil[x]), 
+            float(training_set_oil[x + 1]), 
+            float(v),
+            100+x
+            ])
+        #else: #odd
+            #y_train[x].assign([200+x, diff2, diff1])
+            #X_train[x].assign([float(v), float(str(1) + tf.as_string(training_set_scaled[x])), float(training_set_scaled_dollar[x + 1]), float(training_set_scaled_dollar[x]), float(str(1) + tf.as_string(training_set_scaled[x+1])), 100+x]) """
+        
 
-X_train = tf.reshape(X_train, (X_train.shape[0]//1, 1, 3))
+X_train = tf.reshape(X_train, (X_train.shape[0]//1, 1, 8))
 y_train = tf.reshape(y_train, (y_train.shape[0]//1, 1, 3))
 
 X_train = tf.cast(X_train, dtype=tf.int32)
@@ -98,25 +165,22 @@ from sklearn import linear_model
 regressor = Sequential()
 
 #adding the first LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 2024 , return_sequences = True, input_shape = (1,3)))
+regressor.add(LSTM(units = 8192, return_sequences = True, input_shape = (1,8)))
 #regressor.add(Dropout(0.2))
-""" #adding the second LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 30, return_sequences = True))
+
+#adding the second LSTM layer and some Dropout regularisation
+regressor.add(LSTM(units = 128, return_sequences = True))
 #regressor.add(Dropout(0.2))
 
 #adding the third LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 30, return_sequences = True))
+regressor.add(LSTM(units = 128, return_sequences = True))
 #regressor.add(Dropout(0.2))
 
 #adding the fourth LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 30, return_sequences = True))
+regressor.add(LSTM(units = 128, return_sequences = True))
 #regressor.add(Dropout(0.2))
 
-#adding the fourth LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 30, return_sequences = True))
-#regressor.add(Dropout(0.2))
-
-#adding the fourth LSTM layer and some Dropout regularisation
+""" #adding the fourth LSTM layer and some Dropout regularisation
 regressor.add(LSTM(units = 30, return_sequences = True))
 #regressor.add(Dropout(0.2))
  """
@@ -125,14 +189,14 @@ regressor.add(Dense(units = 3, activation='softmax')) #sigmoid
 
 #compiling the RNN
 regressor.compile(optimizer = 'adam', loss = 'categorical_crossentropy')
-
+#categorical_crossentropy
 """ X_train = tf.one_hot(X_train, 3)
 y_train = tf.one_hot(y_train, 3)
 print("to_categorical x_train: ",X_train)
 print("to_categorical y_train: ",y_train) """
 
 tensorboard = tf.keras.callbacks.TensorBoard(log_dir='logs/')
-regressor.fit(X_train, y_train, epochs = 5000, batch_size=1, callbacks=[tensorboard], verbose=1) #700
+regressor.fit(X_train, y_train, epochs = 50, batch_size=9, callbacks=[tensorboard], verbose=1) #batch_size 9 -> 9/16 = 192 record
 print(regressor.metrics_names)
 # evaluate the model
 scores = regressor.evaluate(X_train, y_train, verbose=0)
